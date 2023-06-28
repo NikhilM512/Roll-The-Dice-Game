a404e0c5a3f7be9e278471083f45699e8c8ce776
Nikhil Magar <nikhilmagar1995@gmail.com>
2023-06-28 08:18:55 +0530

    Sturcture the HTML,CSS & JS seperately

---
+++ Scripts/game.js
@@ -0,0 +1,108 @@
+function rollTheDice(){
+        
+    let n1 = Math.random()*6;
+    n1 = Math.floor(n1);
+
+    let n2 = Math.random()*6;
+    n2 = Math.floor(n2);
+
+    if(n1==0){
+        n1=1
+    }
+    if(n2==0){
+        n2=1
+    }
+    
+    let score1 = document.getElementById("score1");
+
+    let score2 = document.getElementById("score2");
+
+    score1.innerHTML = n1;
+    score2.innerHTML = n2;
+
+    let dice1 = document.getElementById("dice1");
+    dice1.src=`./assests/dice (${n1}).png`
+    let dice2 = document.getElementById("dice2");
+    dice2.src=`./assests/dice (${n2}).png`
+
+    let player1=JSON.parse(localStorage.getItem("Name"));
+    let player2="Anonymous";
+    localStorage.setItem("Player1Name",JSON.stringify(player1));
+    localStorage.setItem("Player2Name",JSON.stringify(player2));
+
+    if(n1>n2){
+        localStorage.setItem("Winner",JSON.stringify(player1));
+        localStorage.setItem("Loser",JSON.stringify(player2));
+        localStorage.setItem("Player1Score",JSON.stringify(n1));
+        localStorage.setItem("Player2Score",JSON.stringify(n2));
+    }else if(n1<n2){
+        localStorage.setItem("Winner",JSON.stringify(player2));
+        localStorage.setItem("Loser",JSON.stringify(player1));
+        localStorage.setItem("Player1Score",JSON.stringify(n1));
+        localStorage.setItem("Player2Score",JSON.stringify(n2));
+        // console.log(n1,n2,"k")
+    }else{
+        localStorage.setItem("Winner",JSON.stringify("DRAW"));
+        localStorage.setItem("Loser",JSON.stringify("DRAW"));
+        localStorage.setItem("Player1Score",JSON.stringify(n1));
+        localStorage.setItem("Player2Score",JSON.stringify(n2));
+    }
+    console.log("Hello")
+    setTimeout(()=>{
+        window.location.href="result.html"
+        // console.log(n1,n2)
+    },1000)
+   
+}
+
+
+addPlayersNames();
+
+function addPlayersNames(){
+
+     let nickname = JSON.parse(localStorage.getItem("Nickname"));
+
+     let heading = document.getElementById("nickname");
+
+     heading.innerText = `Hello ${nickname}👋`;
+
+     let name = JSON.parse(localStorage.getItem("Name"));
+
+     let Navbar_Name = document.getElementById("name");
+
+     Navbar_Name.innerText = `${name}`;
+
+     let Player1_Name = document.getElementById("Name");
+
+     Player1_Name.innerText = `${name}`;
+}
+
+
+// playMusic()
+let music;
+
+document.addEventListener("load", function() {
+    const audio = document.getElementById("background-music");
+    audio.volume = 0.5;
+    audio.play().catch(function(error) {
+    console.error("Failed to play audio:", error);
+    });
+
+});
+
+function toggleMusic(){
+if(!music){
+const audio = document.getElementById("background-music");
+audio.play();
+music=!music;
+let audio_icon = document.getElementById("audio");
+audio_icon.src="./assests/audio-speaker-on.png"
+}else{
+const audio = document.getElementById("background-music");
+audio.pause();
+music=!music;
+let audio_icon = document.getElementById("audio");
+audio_icon.src="./assests/mute.png"
+}
+}
+
